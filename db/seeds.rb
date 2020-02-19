76a9e7196ba664690833008a64ed07096a4aa3c4
Rahul Robertson <rahulrobertson@hotmail.com>
2020-02-19 15:31:08 +0000

    lalalalaa

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+["chinese", "italian", "japanese", "french", "belgian"]
+
+dishoom = {name: 'Dishoom', category: 'french', address: 'London'}
+nandos = {name: 'Nandos', category: 'belgian', address: 'Paris'}
+yuatcha = {name: 'Yuatcha', category: 'chinese', address: 'Kolkata'}
+pizza = {name: 'Pizza Union', category: 'italian', address: 'Hull'}
+atari = {name: 'Atari-Ya', category: 'japanese', address: 'Glasgow'}
+
+restaurants = [dishoom, nandos, yuatcha, pizza, atari]
+
+restaurants.each do |restaurant|
+  Restaurant.create(restaurant)
+end
